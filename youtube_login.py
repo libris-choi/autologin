86d0a8cb0a2d1fb8ccf53763c87410f261be773b
Yongsoon Choi <libris.choi@gmail.com>
2020-05-01 15:40:36 +0900

    Update youtube_login.py
    Case already log-in

--- youtube_login.py
+++ youtube_login.py
@@ -31,15 +31,27 @@ driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%
 driver.implicitly_wait(5)
 
 driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
-driver.find_element_by_xpath('//input[@type="email"]').send_keys('********@gmail.com')
-driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
 driver.implicitly_wait(5)
 
-driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys('********')
-driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
-driver.implicitly_wait(5)
+try:
+    driver.find_element_by_xpath('//input[@type="email"]').send_keys('********@gmail.com')
+    driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
+    driver.implicitly_wait(5)
 
+    driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys('********')
+    driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
+    driver.implicitly_wait(5)
+
+# Already log-in
+except NoSuchElementException:
+    None
+    
 driver.get("https://www.youtube.com/")
 driver.implicitly_wait(5)
 
-driver.find_element_by_xpath('//*[@id="buttons"]/ytd-button-renderer/a').click()
+try:
+    driver.find_element_by_xpath('//*[@id="buttons"]/ytd-button-renderer/a').click()
+    driver.implicitly_wait(5)
+# Already log-in
+except NoSuchElementException:
+    None
